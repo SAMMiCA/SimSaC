43abe282231dc7d8c8cb829a92f9ebc014fa6cb6
chickenbestlover <jmpark@rit.kaist.ac.kr>
2022-08-09 09:42:05 +0900

    add train

--- train.py
+++ train.py
@@ -9,14 +9,13 @@ import pickle
 import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
-from utils_training.optimize_GLUChangeNet import train_epoch, validate_epoch, test_epoch
-from models.our_models.GLUChangeNet import GLUChangeNet_model
-from utils_training.utils_CNN import load_checkpoint, save_checkpoint, boolean_string
-from tensorboardX import SummaryWriter
-from utils.transforms import ArrayToTensor
-from datasets.prepare_dataloaders import prepare_trainval,prepare_test
-from datasets.prepare_transforms import prepare_transforms
+from utils_training.optimize_SimSaC import train_epoch, validate_epoch, test_epoch
 from utils_training.prepare_optimizer import prepare_optim
+from utils_training.utils_CNN import load_checkpoint, save_checkpoint, boolean_string
+from utils.transforms import ArrayToTensor
+from datasets.prepare_dataloaders import prepare_trainval, prepare_test
+from datasets.prepare_transforms import prepare_transforms
+from models.our_models.SimSaC import SimSaC_Model
 
 if __name__ == "__main__":
     # Argument parsing
@@ -25,10 +24,8 @@ if __name__ == "__main__":
     parser.add_argument('--name_exp', type=str,
                         default=time.strftime('%Y_%m_%d_%H_%M'),
                         help='name of the experiment to save')
-    parser.add_argument('--training_data_dir', type=str, default='../dataset/train_datasets',
-                        help='path to directory containing original images for training')
-    parser.add_argument('--evaluation_data_dir', type=str,  default='../dataset/test_datasets',
-                        help='path to directory containing original images for validation')
+    parser.add_argument('--data_dir', type=str, default='../dataset',
+                        help='path to dataset directory for train/eval')
     parser.add_argument('--snapshots', type=str, default='./snapshots')
     parser.add_argument('--pretrained', dest='pretrained',
                         help='path to pre-trained model (load only model params)')
@@ -48,8 +45,6 @@ if __name__ == "__main__":
                         help='number of training epochs')
     parser.add_argument('--train_batch_size', type=int, default=24, # for RTX3090
                         help='train/val batch size')
-    parser.add_argument('--test_batch_size', type=int, default=24, # for RTX3090
-                        help='test batch size')
     parser.add_argument('--n_threads', type=int, default=4,
                         help='number of parallel threads for dataloaders')
     parser.add_argument('--weight-decay', type=float, default=4e-4,
@@ -61,7 +56,7 @@ if __name__ == "__main__":
     parser.add_argument('--split_ratio', type=float, default=0.99,
                         help='train/val split ratio')
     parser.add_argument('--split2_ratio', type=float, default=0.99,
-                        help='val/not-used split ratio (if 0.9, use 90% of val samples)')
+                        help='val/not-used split ratio (if 0.9, use 90%% of val samples)')
     parser.add_argument('--milestones', nargs='+', type=int,
                         default=[12,20,23], # for 25 epoch training schedule
                         help='schedule for learning rate decrease')
@@ -80,6 +75,8 @@ if __name__ == "__main__":
                         help='rgb or bgr')
 
     # Evaluation parameters
+    parser.add_argument('--test_batch_size', type=int, default=24, # for RTX3090
+                        help='test batch size')
     parser.add_argument('--test_img_size', nargs='+', type=int,
                         default=[520,520],
                         help='img_size (if you want to use synthetic dataset, this value should be (520,520)')
@@ -116,17 +113,17 @@ if __name__ == "__main__":
                                     flow_transform=flow_transform, co_transform=None, change_transform=change_transform)
 
     # models
-    model = GLUChangeNet_model(batch_norm=True, pyramid_type='VGG',
-                                 div=args.div_flow, evaluation=False,
-                                 consensus_network=False,
-                                 cyclic_consistency=True,
-                                 dense_connection=True,
-                                 decoder_inputs='corr_flow_feat',
-                                 refinement_at_all_levels=False,
-                                 refinement_at_adaptive_reso=True,
-                                 num_class=5 if args.multi_class else 2,
-                                 use_pac = args.use_pac,
-                                 vpr_candidates=args.vpr_candidates)
+    model = SimSaC_Model(batch_norm=True, pyramid_type='VGG',
+                         div=args.div_flow, evaluation=False,
+                         consensus_network=False,
+                         cyclic_consistency=True,
+                         dense_connection=True,
+                         decoder_inputs='corr_flow_feat',
+                         refinement_at_all_levels=False,
+                         refinement_at_adaptive_reso=True,
+                         num_class=2,
+                         use_pac = False,
+                         vpr_candidates=False)
     print(colored('==> ', 'blue') + 'GLU-Change-Net created.')
 
     # Optimizer
@@ -174,12 +171,10 @@ if __name__ == "__main__":
         start_epoch = 0
 
     # confusionmeter
-    num_class = 5 if args.multi_class else 2
+    num_class = 2
 
     # create summary writer
     save_path = osp.join(args.snapshots, cur_snapshot)
-    train_writer = SummaryWriter(os.path.join(save_path, 'train'))
-    val_writer = SummaryWriter(os.path.join(save_path, 'test'))
 
     model = nn.DataParallel(model)
     model = model.to(device)
@@ -195,14 +190,14 @@ if __name__ == "__main__":
                                      train_dataloader,
                                      device,
                                      epoch,
-                                     train_writer,
+                                     # train_writer,
                                      div_flow=args.div_flow,
                                      save_path=os.path.join(save_path, 'train'),
                                      loss_grid_weights=weights_loss_coeffs)
             scheduler.step()
-            train_writer.add_scalar('train loss: flow(EPE)', train_loss['flow'], epoch)
-            train_writer.add_scalar('train loss: change(FE)', train_loss['change'], epoch)
-            train_writer.add_scalar('learning_rate', scheduler.get_last_lr()[0], epoch)
+            # train_writer.add_scalar('train loss: flow(EPE)', train_loss['flow'], epoch)
+            # train_writer.add_scalar('train loss: change(FE)', train_loss['change'], epoch)
+            # train_writer.add_scalar('learning_rate', scheduler.get_last_lr()[0], epoch)
             print(colored('==> ', 'green') + 'Train average loss:', train_loss['total'])
 
             save_checkpoint({'epoch': epoch + 1,
@@ -215,7 +210,7 @@ if __name__ == "__main__":
             for dataset_name,test_dataloader in test_dataloaders.items():
                 result = test_epoch(args, model, test_dataloader, device, epoch=epoch,
                            save_path=os.path.join(save_path, dataset_name),
-                           writer=val_writer,
+                           # writer=val_writer,
                            div_flow=args.div_flow,
                            plot_interval=args.plot_interval)
                 print('          F1: {:.2f}, Accuracy: {:.2f} '.format(result['f1'], result['accuracy']))
@@ -229,7 +224,7 @@ if __name__ == "__main__":
             result = \
                 validate_epoch(args, model, val_dataloader, device, epoch=epoch,
                                save_path=os.path.join(save_path, 'val'),
-                               writer = val_writer,
+                               # writer = val_writer,
                                div_flow=args.div_flow,
                                loss_grid_weights=weights_loss_coeffs)
             val_loss_grid, val_mean_epe, val_mean_epe_H_8, val_mean_epe_32, val_mean_epe_16  = \
@@ -241,11 +236,11 @@ if __name__ == "__main__":
             print('mean EPE from reso H/8 is {}'.format(val_mean_epe_H_8))
             print('mean EPE from reso 32 is {}'.format(val_mean_epe_32))
             print('mean EPE from reso 16 is {}'.format(val_mean_epe_16))
-            val_writer.add_scalar('validation images: mean EPE ', val_mean_epe, epoch)
-            val_writer.add_scalar('validation images: mean EPE_from_reso_H_8', val_mean_epe_H_8, epoch)
-            val_writer.add_scalar('validation images: mean EPE_from_reso_32', val_mean_epe_32, epoch)
-            val_writer.add_scalar('validation images: mean EPE_from_reso_16', val_mean_epe_16, epoch)
-            val_writer.add_scalar('validation images: val loss', val_loss_grid, epoch)
+            # val_writer.add_scalar('validation images: mean EPE ', val_mean_epe, epoch)
+            # val_writer.add_scalar('validation images: mean EPE_from_reso_H_8', val_mean_epe_H_8, epoch)
+            # val_writer.add_scalar('validation images: mean EPE_from_reso_32', val_mean_epe_32, epoch)
+            # val_writer.add_scalar('validation images: mean EPE_from_reso_16', val_mean_epe_16, epoch)
+            # val_writer.add_scalar('validation images: val loss', val_loss_grid, epoch)
 
             print('          F1: {:.2f}, Accuracy: {:.2f} '.format(result['f1'], result['accuracy']))
             print('          Static  |   Change   |   mIoU ')
@@ -269,7 +264,7 @@ if __name__ == "__main__":
         for dataset_name, test_dataloader in test_dataloaders.items():
             result = test_epoch(args, model, test_dataloader, device, epoch=start_epoch,
                                 save_path=os.path.join(save_path, dataset_name),
-                                writer=val_writer,
+                                # writer=val_writer,
                                 plot_interval=args.plot_interval)
             print('          F1: {:.2f}, Accuracy: {:.2f} '.format(result['f1'], result['accuracy']))
             print('          Static  |   Change   |   mIoU ')
